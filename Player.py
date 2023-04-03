9cb7210337b95422a99bc5641d16434880d470fc
Douglas M Russo <89999372+dmr034@users.noreply.github.com>
2023-04-03 13:28:57 -0400

    Created game window, added player that can move in Player.py.

---
+++ Player.py
@@ -0,0 +1,37 @@
+import pygame
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -1)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 1)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-1, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(1, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
--- main.py
+++ main.py
@@ -3,5 +3,46 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import pygame
+from Player import Player
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
 if __name__ == '__main__':
-    print("hi")
+    pygame.init()
+
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+    player = Player()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE: # If escape key is pressed, exit the program
+                    running = False
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((0, 0, 0))
+
+        screen.blit(player.surf, player.rect)
+
+        pressed_keys = pygame.key.get_pressed()
+        player.update(pressed_keys)
+
+        pygame.display.flip()
+
+    # Done! Time to quit.
+    pygame.quit()
