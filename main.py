5f8741c141f80f5a7f9582f0764eb649ff3bb7a0
Douglas M Russo <89999372+dmr034@users.noreply.github.com>
2023-04-03 12:37:52 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+if __name__ == '__main__':
+    print("hi")
